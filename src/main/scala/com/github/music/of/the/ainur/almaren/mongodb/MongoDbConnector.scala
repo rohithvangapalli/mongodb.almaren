79a7ee7c13a5ec525e03183cc8a87cd627f673b1
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2020-05-11 13:54:41 +0200

    add source for mongo

--- src/main/scala/com/github/music/of/the/ainur/almaren/mongodb/MongoDbConnector.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/mongodb/MongoDbConnector.scala
@@ -6,35 +6,55 @@ import com.github.music.of.the.ainur.almaren.builder.Core
 import com.github.music.of.the.ainur.almaren.state.core.{Target,Source}
 import com.mongodb.spark._
 import com.mongodb.spark.config._
+import com.mongodb.spark.sql.SparkSessionFunctions
 
-private[almaren] case class SourceMongoDb(uri: String, options:Map[String,String]) extends Source {
+private[almaren] case class SourceMongoDb(
+  hosts: String,
+  database: String, 
+  collection: String,
+  user:Option[String],
+  password:Option[String],
+  options:Map[String,String]) extends Source {
+
   def source(df: DataFrame): DataFrame = {
-    logger.info(s"uri:{$uri}, options:{$options}")
-    df.sparkSession.read.format("mongo")
-      .options(options)
-      .load
-  }
+    logger.info(s"hosts:{$hosts}, database:{$database}, collection:{$collection}, user:{$user}, options:{$options}")
+    SparkSessionFunctions(df.sparkSession).loadFromMongoDB(ReadConfig(
+     (user,password) match {
+       case (Some(u),Some(p)) => Map("uri" -> s"mongodb://$u:$p@$hosts/$database.$collection") ++ options
+       case (_,_) => Map("uri" -> s"mongodb://$hosts/$database.$collection") ++ options
+     })
+    )}
 }
 
-private[almaren] case class TargetMongoDb(uri: String, options:Map[String,String],saveMode:SaveMode) extends Target {
+
+private[almaren] case class TargetMongoDb(
+  hosts: String,
+  database: String, 
+  collection: String,
+  user:Option[String],
+  password:Option[String],
+  options:Map[String,String],
+  saveMode:SaveMode) extends Target {
+
   def target(df: DataFrame): DataFrame = {
-    logger.info(s"uri:{$uri}, saveMode:{$saveMode}, options:{$options}")
+    logger.info(s"hosts:{$hosts}, database:{$database}, collection:{$collection}, user:{$user}, options:{$options}, saveMode:{$saveMode}")
     df.write.format("mongo")
       .options(options)
       .mode(saveMode)
       .save
     df
   }
+
 }
 
 private[almaren] trait MongoDbConnector extends Core {
-  def targetMongoDb(uri: String, options:Map[String,String] = Map(),saveMode:SaveMode = SaveMode.ErrorIfExists): Option[Tree] =
-     TargetMongoDb(uri,options,saveMode)
+  def targetMongoDb(hosts: String,database: String,collection: String,user:Option[String] = None,password:Option[String] = None,options:Map[String,String] = Map(),saveMode:SaveMode = SaveMode.ErrorIfExists): Option[Tree] =
+     TargetMongoDb(hosts,database,collection,user,password,options,saveMode)
 
-  def sourceMongoDb(uri: String, options:Map[String,String] = Map()): Option[Tree] =
-    SourceMongoDb(uri,options)
+  def sourceMongoDb(hosts: String,database: String,collection: String,user:Option[String] = None,password:Option[String] = None,options:Map[String,String] = Map()): Option[Tree] =
+    SourceMongoDb(hosts,database,collection,user,password,options)
 }
 
 object MongoDb {
-  implicit class Implicit(val container: Option[Tree]) extends MongoDbConnector
+  implicit class MongoImplicit(val container: Option[Tree]) extends MongoDbConnector
 }
---
+++ src/test/scala/com/github/music/of/the/ainur/almaren/mongodb/Test.scala
@@ -0,0 +1,29 @@
+package com.github.music.of.the.ainur.almaren.mongodb
+
+import org.apache.spark.sql.SaveMode
+import org.scalatest._
+import com.github.music.of.the.ainur.almaren.Almaren
+import com.github.music.of.the.ainur.almaren.builder.Core.Implicit
+import com.github.music.of.the.ainur.almaren.mongodb.MongoDb.MongoImplicit
+
+class Test extends FunSuite with BeforeAndAfter {
+
+  val almaren = Almaren("App Test")
+
+  val spark = almaren.spark
+    .master("local[*]")
+    .config("spark.ui.enabled","false")
+    .config("spark.sql.shuffle.partitions", "1")
+    .getOrCreate()
+  
+  spark.sparkContext.setLogLevel("ERROR")
+
+  import spark.implicits._
+
+  // Save Twitter data to Solr
+  val df = almaren.builder.sourceMongoDb("localhost","foo","movie").batch
+
+  df.show(false)
+  df.printSchema()
+
+}
