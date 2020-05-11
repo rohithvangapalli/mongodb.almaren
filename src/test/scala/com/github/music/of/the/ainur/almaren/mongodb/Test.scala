46ef86f3eedd6e9d87cb629986fb8eb882e854a0
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2020-05-11 19:03:06 +0200

    add test to mongodb

--- src/test/scala/com/github/music/of/the/ainur/almaren/mongodb/Test.scala
+++ src/test/scala/com/github/music/of/the/ainur/almaren/mongodb/Test.scala
@@ -5,6 +5,9 @@ import org.scalatest._
 import com.github.music.of.the.ainur.almaren.Almaren
 import com.github.music.of.the.ainur.almaren.builder.Core.Implicit
 import com.github.music.of.the.ainur.almaren.mongodb.MongoDb.MongoImplicit
+import org.apache.spark.sql.{DataFrame, Column}
+import org.apache.spark.sql.AnalysisException
+import org.apache.spark.sql.functions._
 
 class Test extends FunSuite with BeforeAndAfter {
 
@@ -20,14 +23,61 @@ class Test extends FunSuite with BeforeAndAfter {
 
   import spark.implicits._
 
-  // Save Twitter data to Solr
-  val df = almaren.builder.sourceMongoDb("localhost","foo","movie").batch
+  val testTable = "movies"
+  createSampleData(testTable)
 
-  df.createOrReplaceTempView("foo")
+  //Write Data From MongoDB
+  val df1 = almaren.builder.sourceSql(s"SELECT * FROM $testTable").targetMongoDb("localhost","test","movie").batch
 
-  almaren.builder.sourceSql("SELECT * FROM foo").targetMongoDb("localhost","baz","movie").batch
+  // Read Data From MongoDB
+  val df2 = almaren.builder.sourceMongoDb("localhost","test","movie").batch
 
-  df.show(false)
-  df.printSchema()
+  test(df1,df2,"MongoDB")
+
+  def test(df1: DataFrame, df2: DataFrame, name: String): Unit = {
+    testCount(df1, df2, name)
+    testCompare(df1, df2, name)
+  }
+
+  def testCount(df1: DataFrame, df2: DataFrame, name: String): Unit = {
+    val count1 = df1.count()
+    val count2 = df2.count()
+    val count3 = spark.emptyDataFrame.count()
+    test(s"Count Test:$name should match") {
+      assert(count1 == count2)
+    }
+    test(s"Count Test:$name should not match") {
+      assert(count1 != count3)
+    }
+  }
+
+  // Doesn't support nested type and we don't need it :)
+  def testCompare(df1: DataFrame, df2: DataFrame, name: String): Unit = {
+    val diff = compare(df1, df2)
+    test(s"Compare Test:$name should be zero") {
+      assert(diff == 0)
+    }
+    test(s"Compare Test:$name, should not be able to join") {
+      assertThrows[AnalysisException] {
+        compare(df2, spark.emptyDataFrame)
+      }
+    }
+  }
+
+  private def compare(df1: DataFrame, df2: DataFrame): Long =
+    df1.as("df1").join(df2.as("df2"), joinExpression(df1), "leftanti").count()
+
+  private def joinExpression(df1: DataFrame): Column =
+    df1.schema.fields
+      .map(field => col(s"df1.${field.name}") <=> col(s"df2.${field.name}"))
+      .reduce((col1, col2) => col1.and(col2))
+
+
+  def createSampleData(tableName: String): Unit = {
+    val json_str = scala.io.Source.fromURL(getClass.getResource("/sample_data/movies.json")).mkString
+    val res = spark.read.json(Seq(json_str).toDS)
+    res.createTempView(tableName)
+  }
+
 
 }
